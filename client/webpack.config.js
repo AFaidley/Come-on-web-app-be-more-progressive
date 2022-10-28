496e329fa362485c9abc0dfe8dc16d821ce677e8
AFaidley <alviva.faidley@gmail.com>
2022-10-27 19:34:09 -0600

    Added css loader and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,38 +22,54 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE',
-    }),
+      }),
 
-// Injects custome service worker
-    new InjectManifest({
-      swSrc: './src-sw.js',
-      swDest: 'src-sw.js',
-  }),
+      // Injects custome service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
-  // Create manifest.json
-  new WebpackPwaManifest({
-    fingerprints: false,
-    inject: true,
-    name: 'The wonderful world of text editing',
-    short_name: 'TextEditor',
-    descrption: 'Time to edit some text!',
-    background_color: 'black',
-    start_url: './',
-    publicPath: './',
-    icons: [
-        {
+      // Create manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'The wonderful world of text editing',
+        short_name: 'TextEditor',
+        descrption: 'Time to edit some text!',
+        background_color: 'black',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
-        },
-    ],
-}),
-      
+          },
+        ],
+      }),
+
     ],
 
     module: {
       rules: [
-        
+        // CSS loader and babel
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+
       ],
     },
   };
