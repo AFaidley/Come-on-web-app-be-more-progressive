b99ff696bdf7a1c95c5966be82eaada56615a859
AFaidley <alviva.faidley@gmail.com>
2022-10-27 20:12:19 -0600

    Worked on PUTDB func, yet to do GETDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('PUT from DB');
+
+  // Create connection to DB
+  const jateDB = await openDB('jate', 1);
+    
+  // Create new transaction and state the DB and data privileges
+  const tx = jateDB.transaction('jate', 'readwrite');
+  
+  // Open objectstore
+  const store = tx.objectStore('jate');
+
+  // Update
+  const request = store.put({ jate: content });
+  
+  // Confirm req
+  const result = await request;
+  console.log('Data has been saved in the database', result);
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('GET from DB');
+
+}
 
 initdb();
