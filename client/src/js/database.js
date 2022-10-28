1ef64b6b6dad6d6973f5aca36e9f5f34bf44ef27
AFaidley <alviva.faidley@gmail.com>
2022-10-27 20:52:30 -0600

    Completed GETDB func

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,26 +18,40 @@ export const putDb = async (content) => {
 
   // Create connection to DB
   const jateDB = await openDB('jate', 1);
-    
+
   // Create new transaction and state the DB and data privileges
   const tx = jateDB.transaction('jate', 'readwrite');
-  
+
   // Open objectstore
   const store = tx.objectStore('jate');
 
   // Update
   const request = store.put({ jate: content });
-  
+
   // Confirm req
   const result = await request;
   console.log('Data has been saved in the database', result);
-
-}
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('GET from DB');
 
-}
+  // Create connection to DB
+  const jateDB = await openDB("jate", 1);
+
+// Create new transaction and state the DB and data privileges
+  const tx = jateDB.transaction("jate", "readonly");
+
+  // Open objectstore
+  const store = tx.objectStore("jate");
+
+  // Get all data
+  const request = store.getAll();
+
+  // Confirm req
+  const result = await request;
+  console.log(result);
+};
 
 initdb();
